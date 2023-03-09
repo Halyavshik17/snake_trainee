2313eeaa92b09d5ea152cf6a0b96dd016c7c41a0
Halyavshik17 <zerotwofollow@gmail.com>
2023-03-09 12:34:30 +0300

    Snake trainee

---
+++ script.js
@@ -0,0 +1,115 @@
+// first relese 1.0 
+// need update 
+// pause = shift, score = speed snake, more food on map, menu start and gameover (delete alert())
+
+const playBoard = document.querySelector(".play-board");
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
+const controls = document.querySelectorAll(".controls i");
+
+const startgame = document.querySelector(".btn-style");
+const textGameOver = document.querySelector(".play-gameover");
+
+let gameStart = false;
+let gameOver = false; 
+let foodX, foodY;
+let snakeX = 5, snakeY = 5;
+let velocityX = 0, velocityY = 0;
+let snakeBody = [];
+let setIntervalId;
+let score = 0;
+
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreElement.innerText = `High Score: ${highScore}`;
+
+const updateFoodPosition = () => {
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+const handleGameOver = () => {
+    gameStart = false;
+
+    // startgame.style.display = "block";
+    // playBoard.style.display = "none";
+
+    textGameOver.style.display = "block"
+
+    clearInterval(setIntervalId);
+    alert("Game Over!");
+    location.reload();
+}
+
+// ловим нажатие кнопки button
+startgame.onclick = () => {
+    gameStart = true;
+
+    // запуск движения змейки -> ArrowRight
+    velocityX = 1;
+    velocityY = 0;
+
+    startgame.style.display = "none";
+    playBoard.style.display = "grid";
+};
+
+const changeDirection = e => {
+    if(e.key === "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } 
+    else if(e.key === "ArrowDown" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if(e.key === "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0
+    }
+    else if(e.key === "ArrowRight" && velocityX != 1) {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+// controls.forEach(button => button.addEventListener('click', () => changeDirection({key: button.dataset.key})));
+const initGame = () => {
+    if(gameStart == false) return;
+    if(gameOver) return handleGameOver();
+    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+    if(snakeX === foodX && snakeY === foodY) {
+        updateFoodPosition();
+        snakeBody.push([foodY, foodX]);
+        score++;
+        highScore = score >= highScore ? score : highScore;
+
+        localStorage.setItem("high-score", highScore);
+        scoreElement.innerText = `Score: ${score}`;
+        highScoreElement.innerText = `High Score: ${highScore}`;
+    }
+
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    for(let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+
+    snakeBody[0] = [snakeX, snakeY];
+
+    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+        return gameOver = true;
+    }
+
+    for(let i = 0; i < snakeBody.length; i++) {
+        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+        if(i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+            gameOver = true;
+        }
+    }
+    playBoard.innerHTML = html;
+}
+
+updateFoodPosition();
+setIntervalId = setInterval(initGame, 100);
+document.addEventListener("keyup", changeDirection);
